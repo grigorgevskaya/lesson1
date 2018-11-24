f196b654e29b562cb026de7a8e96c4f346986250
grigorgevskaya <okosa8@gmail.com>
2018-11-24 17:29:26 +0300

    First commit

---
+++ info.py
@@ -0,0 +1,6 @@
+user_name = {"first_name": "Ольга", "last_name": "Иванова"}
+print(user_name["first_name"])
+
+
+
+
---
+++ vat.py
@@ -0,0 +1,11 @@
+def get_summ(one,two,deliminer="&"):
+    return one.upper()+deliminer+two.upper()
+a=get_summ("learn","python")
+print(a)
+
+	
+
+
+
+
+
